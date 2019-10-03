75bc9e0379dcec32a8b1d0d3aa7ac3e20469ef5b
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2019-10-03 18:14:39 -0400

    mvp complete

---
+++ testing/src/components/Display.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Display = props => {
+  return (
+    <div className="display">
+      <p>{props.displayType}</p>
+      <span>{props.displayCount}</span>
+    </div>
+  );
+};
+
+export default Display;
